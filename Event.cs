51dd5edc4187735380a2ea48f4dc78d5e96a1cb4
Roma <kompas272@gmail.com>
2024-12-27 14:21:50 +0200

    додали Docker support

---
+++ Event.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRPZ3
+{
+    public class Eventt
+    {
+        public string Name { get; }
+        public string StartDate { get; }
+        public string EndDate { get; }
+        public string Location { get; }
+
+        public string status;
+
+        public string Status
+        {
+            get => status;
+            set
+            {
+                if (value == "Заплановано" || value == "Виконується" || value == "Завершено")                
+                    status = value;
+                
+                else                
+                    throw new ArgumentException("Невірний статус. Дозволені статуси: 'Заплановано', 'Виконується', 'Завершено'");                
+            }
+        }
+        public Eventt(string name, string startDate, string location, string status)
+        {
+            Name = name;
+            StartDate = startDate;
+            Location = location;
+            Status = status; 
+        }
+    }
+}
--- Route.cs
+++ Route.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace MRPZ3
 {
@@ -12,14 +13,15 @@ namespace MRPZ3
         public string StartDate { get; }
         public string EndDate { get; }
         public List<string> Destinations { get; }
-
+        public List<Eventt> Eventss { get; } = new();
         public Route(string name, string startDate, string endDate)
         {
+            Eventss = new List<Eventt>();
             Name = name;
             StartDate = startDate;
             EndDate = endDate;
             Destinations = new List<string>();
-        }
+        } 
         public void AddDestination(string destination)
         {   
             if (!Destinations.Contains(destination))
@@ -34,5 +36,30 @@ namespace MRPZ3
                 Destinations.Remove(destination);
             }
         }
+        public void AddEventt(Eventt ev)
+        {
+            if (!Eventss.Contains(ev))
+            {
+                Eventss.Add(ev);
+            }          
+        }
+        public void UpdateEventStatus(string eventName, string newStatus)
+        {            
+            var eventt = Eventss.FirstOrDefault(e => e.Name == eventName);
+
+            if (eventt == null)
+            {
+                throw new ArgumentException($"Подію з назвою '{eventName}' не знайдено.");
+            }
+
+            if (newStatus != "Заплановано" && newStatus != "Виконується" && newStatus != "Завершено")
+            {
+                throw new ArgumentException("Невірний статус. Дозволені статуси: 'Заплановано', 'Виконується', 'Завершено'.");
+            }
+            eventt.Status = newStatus;
+        }
+
+       
+       
     }
 }
